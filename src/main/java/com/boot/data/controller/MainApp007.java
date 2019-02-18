87817c3d2f2b951ad77c17601b54bdc14edbd559
CoachK333 <985482158@qq.com>
2019-02-18 15:09:07 +0800

    不重要的

--- src/main/java/com/boot/data/controller/MainApp007.java
+++ src/main/java/com/boot/data/controller/MainApp007.java
@@ -14,24 +14,7 @@ import java.util.Date;
  */
 public class MainApp007 {
     public static void main(String[] args) throws ParseException {
-
-
-        LocalDateTime dateTime = LocalDateTime.parse("2018-12-01 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        int hour = dateTime.getHour();
-        System.out.println(hour);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = sdf.parse("2018-12-01 00:00:00");
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        if (instance.get(Calendar.HOUR_OF_DAY) == 0 && instance.get(Calendar.MINUTE) == 0 && instance.get(Calendar.SECOND) == 0) {
-            instance.add(Calendar.MILLISECOND, -1);
-            Date time = instance.getTime();
-            System.out.println(sdf.format(time));
-
-        }
-
-
+        String s1 = new String();
     }
 
 
