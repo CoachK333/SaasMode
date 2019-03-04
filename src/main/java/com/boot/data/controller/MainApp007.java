5bcf7c456af84aabd8cd10f812674ec5287a0465
CoachK333 <985482158@qq.com>
2019-03-04 19:26:16 +0800

    '添加测试类'

---
+++ src/main/java/com/boot/data/controller/MainApp007.java
@@ -0,0 +1,13 @@
+package com.boot.data.controller;
+
+/**
+ * @author 98548
+ * @create 2019-03-04 12:15
+ * @description
+ */
+public class MainApp007 {
+    public static void main(String[] args) {
+        System.out.println("航喝红酒");
+
+    }
+}
