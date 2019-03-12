c3c99d31bd6177f740e3bede8b6399a59e655072
CoachK333 <985482158@qq.com>
2019-03-12 09:24:14 +0800

    qnmd

---
+++ src/main/java/com/boot/data/controller/MainApp011.java
@@ -0,0 +1,30 @@
+package com.boot.data.controller;
+
+import com.boot.data.util.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * @author 98548
+ * @create 2018-12-03 18:44
+ * @description
+ */
+@Slf4j
+public class MainApp011 {
+
+    public static void main(String[] args) throws UnsupportedEncodingException, MalformedURLException {
+        List<Map<String, String>> list = new ArrayList<>();
+
+    }
+
+}
