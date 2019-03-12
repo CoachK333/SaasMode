5ae4deb2cfef7551b4db6f21629d2ee3e2dd7372
CoachK333 <985482158@qq.com>
2019-03-12 09:27:35 +0800

    cnmde

---
+++ src/main/java/com/boot/data/controller/MainApp012.java
@@ -0,0 +1,17 @@
+package com.boot.data.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 98548
+ * @create 2018-12-03 18:44
+ * @description
+ */
+@Slf4j
+public class MainApp012 {
+
+    public static void main(String[] args) {
+        System.out.println("0001");
+    }
+
+}
