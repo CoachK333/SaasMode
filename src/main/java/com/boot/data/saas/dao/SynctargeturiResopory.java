5e30677f5e9eea113d770a4bf611f5cbdd5ec2bd
CoachK333 <985482158@qq.com>
2019-03-14 14:00:05 +0800

    delete saas code

--- src/main/java/com/boot/data/saas/dao/SynctargeturiResopory.java
+++
@@ -1,9 +0,0 @@
-package com.boot.data.saas.dao;
-
-import com.boot.data.saas.entiy.Synctargeturi;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface SynctargeturiResopory extends JpaRepository<Synctargeturi, String> {
-
-}
